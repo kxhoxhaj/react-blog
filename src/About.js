10c6a16cf241cdda2bc0dffce9a6a7b87e998938
Klevion Xhoxhaj <klevionxhoxhaj@gmail.com>
2024-10-22 22:01:09 +0200

    Completed React JS Blog Tutorial

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='About'>
+      <h2>About</h2>
+      <p>This blog is part of React learning tutorials!</p>
+    </main>
+  )
+}
+
+export default About
